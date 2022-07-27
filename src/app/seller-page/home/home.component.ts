70c1693dbd17eee6f7bea43912768437f33c1021
Parth Bali <pabali@deloitte.com>
2022-07-27 09:28:01 +0530

    Deployed the code on vercel and added vercelignore file

--- src/app/seller-page/home/home.component.ts
+++ src/app/seller-page/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
@@ -9,39 +10,53 @@ import { WishlistService } from 'src/app/services/wishlist.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  products!: Product[];
+
+  products: any[] = [];
+  u_id = localStorage.getItem("id")
+  url = "http://localhost:3000/products/"
+
   constructor(
     // public products: ProductsModel
     private service: ProductService,
-    private wishlistService: WishlistService
+    private wishlistService: WishlistService,
+    private http: HttpClient
   ) {
 
   }
   ngOnInit() {
-    this.service.getProduct().subscribe(
-      data => {
-        this.products = data
-      }
-    )
-    this.service.getProductById("1").subscribe(
-      data => {
-        console.log("INIT", data)
-      }
-    )
+    this.getProduct()
+
   }
-  // public cartflag: boolean = false;
-  // public sortBy: string = '';
-  // public sortOption: string = 'product_name|asc';
-  // ref() {
-  //   this.cartflag = false;
-  //   setTimeout(() => {
-  //     this.cartflag = true;
-  //   }, 10)
-  // }
+
+
   addProductWishlist(product: any) {
     this.wishlistService.addWishlist(product.id)
   }
   openBuyDialog(product: any) {
 
   }
+
+  getProduct() {
+    this.products = []
+    this.service.getProductByOwner().subscribe(
+      (data: any) => {
+        data.forEach((v: Product, i: any) => {
+          if (v.owner == this.u_id) {
+            this.products.push(v)
+          }
+        })
+      }
+    )
+  }
+
+  removeProduct(product: any) {
+    this.http.delete(this.url + product.id).subscribe(
+      data => {
+        this.ngOnInit()
+      }, error => {
+        console.log(error)
+      }
+    )
+  }
+
 }
